59219fccd1e5e8754a1d83fe19fab53fe6aadaf4
JorritBosma <bosma.jorrit@gmail.com>
2020-12-09 11:24:23 +0100

    first commit

---
+++ idee.js
@@ -0,0 +1,67 @@
+
+
+
+// BOSMA validating form klus aanmaken
+// const validateKlus = (req, res, next) => {
+//     const { error } = KlussenSchema.validate(req.body);
+//     if (error) {
+//         const msg = error.details.map(el => el.message).join(',')
+//         throw new ExpressError(msg, 400)
+//     } else {
+//         next();
+//     }
+// };
+// 
+// 
+
+// app.get('/:id', wrapAsync(async (req, res) => {
+//     const wandeling = await (await Wandeling.findById(req.params.id).populate('recensies'));
+//     res.render('wandelingen/show', { wandeling });
+// }));
+
+// app.get('/:id/edit', wrapAsync(async (req, res) => {
+//     const wandeling = await Wandeling.findById(req.params.id)
+//     res.render('wandelingen/edit', { wandeling });
+// }));
+
+// app.put('/:id', validateWandeling, wrapAsync(async (req, res) => {
+//     const { id } = req.params;
+//     const wandeling = await Wandeling.findByIdAndUpdate(id, { ...req.body.wandeling });
+//     res.redirect(`/wandelingen/${wandeling._id}`)
+// }));
+
+// app.delete('/:id', wrapAsync(async (req, res) => {
+//     const { id } = req.params;
+//     await Wandeling.findByIdAndDelete(id);
+//     res.redirect('/wandelingen');
+// }));
+
+
+
+// app.get('/:id/mijn-account', async (req, res) => {
+//     const { gebruiker } = req.params;
+//     const mijnKlussen = KlussenDb.find({ aanvrager: })
+//     res.render('mijn-account', { gebruiker, mijnKlussen });
+// })
+
+
+
+// 
+
+app.get('/aangemeld', (req, res) => {
+    res.render('klus-lijst');
+})
+// 
+
+
+
+<div class="container">
+
+<div class="cat"><img src="/css/pic/Boodschappen.jpg" alt="Boodschappen"></div>
+<div class="cat"><img src="/css/pic/Culinair.jpg" alt="Culinair"></div>
+<div class="cat"><img src="/css/pic/Fysiek.jpg" alt="Fysiek"></div>
+<div class="cat"><img src="/css/pic/Hands.jpg" alt="Hands"></div>
+<div class="cat"><img src="/css/pic/ICT.jpg" alt="ICT"></div>
+<div class="cat"><img src="/css/pic/Sociaal.jpg" alt="Sociaal"></div>
+
+</div>
